44ab50afa93c2ff760f027a727c621ebcec488c2
portable <emmanueltemitopedorcas20@gmail.com>
2025-06-01 15:56:11 -0700

    Refactor UpdateLabTestDto to extend from LabTest and include optional parameters

--- src/lab/dto/update-lab-test.dto.ts
+++ src/lab/dto/update-lab-test.dto.ts
@@ -1,4 +1,7 @@
 import { PartialType } from '@nestjs/mapped-types';
-import { CreateLabTestDto } from './create-lab-test.dto';
+import { DeepPartial } from 'typeorm';
+import { LabTest } from '../entities/lab-test.entity';
 
-export class UpdateLabTestDto extends PartialType(CreateLabTestDto) {}
+export class UpdateLabTestDto extends PartialType(LabTest) {
+  parameters?: DeepPartial<Record<string, any>>;
+}
